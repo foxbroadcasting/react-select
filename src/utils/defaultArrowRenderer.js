020bdc900dc2ad79832e0ecc47bad3d144dac795
Sergio Baez <sergio.baez@wizeline.com>
2018-11-20 11:47:28 -0800

    chore: DPP-1231 Remove up chevron icon

--- src/utils/defaultArrowRenderer.js
+++ src/utils/defaultArrowRenderer.js
@@ -7,11 +7,7 @@ export default function arrowRenderer ({ onMouseDown, isOpen }) {
 			onMouseDown={onMouseDown}
 		>
       <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet">
-        { isOpen ?
-          <g><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6" /></g>
-          :
-          <g><path d="M7.41 8.84L12 13.42l4.59-4.58L18 10.25l-6 6-6-6z" /></g>
-        }
+        <g><path d="M7.41 8.84L12 13.42l4.59-4.58L18 10.25l-6 6-6-6z" /></g>
       </svg>
     </span>
 	);
