850d238f48af4507264d95822bfbf53e04b3484c
Sergio Baez <sergio.baez@wizeline.com>
2018-11-20 15:42:27 -0800

    chore: DPP-1231 Update tab identation

--- src/utils/defaultArrowRenderer.js
+++ src/utils/defaultArrowRenderer.js
@@ -6,10 +6,10 @@ export default function arrowRenderer ({ onMouseDown }) {
 		<span
 			onMouseDown={onMouseDown}
 		>
-      <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet">
-        <g><path d="M7.41 8.84L12 13.42l4.59-4.58L18 10.25l-6 6-6-6z" /></g>
-      </svg>
-    </span>
+			<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet">
+				<g><path d="M7.41 8.84L12 13.42l4.59-4.58L18 10.25l-6 6-6-6z" /></g>
+			</svg>
+		</span>
 	);
 };
 
