909e720028c0873936876d82f3021605a7d9eeb4
Sergio Baez <sergio.baez@wizeline.com>
2018-11-20 15:23:57 -0800

    chore: DPP-1231 Remove parameter in the arrow render

--- src/utils/defaultArrowRenderer.js
+++ src/utils/defaultArrowRenderer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function arrowRenderer ({ onMouseDown, isOpen }) {
+export default function arrowRenderer ({ onMouseDown }) {
 	return (
 		<span
 			onMouseDown={onMouseDown}
